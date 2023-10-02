0255df8c3e109c5d3d154c2e34c4a4f819b5572a
BuildTools <torbotax1@gmail.com>
2023-10-02 02:08:35 -0700

    U

--- mods/PracticeCodes/src/hud.c
+++ mods/PracticeCodes/src/hud.c
@@ -6,14 +6,14 @@ CapitalTextInfo text_info[3] = {{0}};
 void HUDHook()
 {
     
-    //if(_gameState == GAMESTATE_INVENTORY)
-    //{
-        text_info[0].x = 0x1b8;
+    if(_gameState == GAMESTATE_INVENTORY)
+    {
+        text_info[0].x = 0x198;
         text_info[0].y = 0x2c;
         text_info[0].size = 0x1200;
 
-        DrawTextCapitals("TESTING", &text_info[0], DEFAULT_SPACING, MOBY_COLOR_GOLD);
+        DrawTextCapitals("TESTING", &text_info[0], 0x10, MOBY_COLOR_GOLD);
 
         MobyRender();
-    //}
+    }
 }

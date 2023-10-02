3c1983be6e0fce0dc3ff028e9af87406b6211530
BuildTools <torbotax1@gmail.com>
2023-10-01 23:39:05 -0700

    U

--- mods/PracticeCodes/src/main.c
+++ mods/PracticeCodes/src/main.c
@@ -2,8 +2,15 @@
 #include "myfuncs.h"
 #include "levelselect.h"
 
-bool hasSkippedIntro = FALSE;
-bool hasUnlockedLevels = FALSE;
+enum ModState
+{
+    GAME_STARTED,
+    SKIPPED_INTRO,
+    UNLOCKED_LEVELS
+};
+typedef enum ModState ModState;
+
+ModState mod_state = GAME_STARTED;
 
 //Shows all levels in inventory menu, and automatically unlocks homeworlds for balloonists
 void UnlockAllLevels()
@@ -77,51 +84,55 @@ void MainFunc()
     LevelSelect();
     InstaLoad();
 
-    //Run once upon starting
-    if(!hasSkippedIntro && _globalTimer > 20)                //? If the code hasn't ran once yet, and the global timer is greater than 20. Checking global timer because I have to wait a few frames to call The Adventure Begins
+    //Run once upon starting game
+    if(mod_state == GAME_STARTED && _globalTimer > 20)                //? If the code hasn't ran once yet, and the global timer is greater than 20. Checking global timer because I have to wait a few frames to call The Adventure Begins
     {
         SkipIntro();
-        hasSkippedIntro = TRUE;
+        mod_state = SKIPPED_INTRO;
     }
 
     //Run once upon skipping intro
-    if(_isPastTitleScreen && !hasUnlockedLevels)
+    if(mod_state == SKIPPED_INTRO)
     {
         UnlockAllLevels();
         SetTitleScreenColor(70, 0);
-        hasUnlockedLevels = TRUE;
+        mod_state = UNLOCKED_LEVELS;
     }
     
-    //Save/Load spyro & camera information
-    if(_currentButtonOneFrame == L3_BUTTON)
+    //Main Loop
+    if(mod_state == UNLOCKED_LEVELS)
     {
-        SaveSpyroAndCamera();
-    }
-    if(_currentButtonOneFrame == R3_BUTTON)
-    {
-        ReloadSpyroAndCamera();
-    }
+        //Save/Load spyro & camera information
+        if(_currentButtonOneFrame == L3_BUTTON)
+        {
+            SaveSpyroAndCamera();
+        }
+        if(_currentButtonOneFrame == R3_BUTTON)
+        {
+            ReloadSpyroAndCamera();
+        }
 
-    //Respawn spyro & reset level gems
-    if(_currentButton == L1_BUTTON + R1_BUTTON + CIRCLE_BUTTON)
-    {
-        RespawnSpyro();
-    }
-    if(_currentButton == L1_BUTTON + R1_BUTTON + CIRCLE_BUTTON || _movementSubState == MOVEMENT_SUBSTATE_LOADING || _gameState == GAMESTATE_DEATH)
-    {
-        ResetLevelCollectables();
-    }
+        //Respawn spyro & reset level gems
+        if(_currentButton == L1_BUTTON + R1_BUTTON + CIRCLE_BUTTON)
+        {
+            RespawnSpyro();
+        }
+        if(_currentButton == L1_BUTTON + R1_BUTTON + CIRCLE_BUTTON || _movementSubState == MOVEMENT_SUBSTATE_LOADING || _gameState == GAMESTATE_DEATH)
+        {
+            ResetLevelCollectables();
+        }
 
-    //Make Nestor Skippable
-    if(_levelID == ARTISANS_ID)
-    {
-        _isNestorUnskipable = FALSE;
-    }
+        //Make Nestor Skippable
+        if(_levelID == ARTISANS_ID)
+        {
+            _isNestorUnskipable = FALSE;
+        }
 
-    //Moonjump
-    if(_currentButton == L1_BUTTON + R1_BUTTON + X_BUTTON)
-    {
-        _spyro.position.z += 500;
+        //Moonjump
+        if(_currentButton == L1_BUTTON + R1_BUTTON + X_BUTTON)
+        {
+            _spyro.position.z += 500;
+        }
     }
 
 }

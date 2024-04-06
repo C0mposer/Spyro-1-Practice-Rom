f1d37b19b5a66cb6b269e0aff3509b4e80429e83
OddKara <botart9@gmail.com>
2024-04-06 01:00:39 -0700

    cd load these testicals

--- mods/PracticeCodes/src/cd_load.c
+++ mods/PracticeCodes/src/cd_load.c
@@ -1,16 +1,15 @@
 #include <cd.h>
 #include <custom_text.h>
 
-//to avoid making a GemRedistributer.h
-void LoadCDModData();
 
 bool hasFullyLoaded = FALSE;
-bool hasStartedLoad = FALSE;
+int hasStartedLoad = 0;
+//CdlCB callback;
 
 
-#define AMOUNT_OF_SECTORS 1                                                         //? The amount of sectors the mod takes up currently
+#define AMOUNT_OF_SECTORS 2                                                         //? The amount of sectors the mod takes up currently
 #define SECTOR_NUMBER 265759                                                       //? The sector number for my mod.bin on the disk
-#define FREE_RAM_AREA 0x80A50000                                                    //? The free area in the header of ram we are loading into
+#define FREE_RAM_AREA 0x80008BC0                                                   //? The free area in the header of ram we are loading into
 #define GNORC_WALKED_UP_HILL 2      
                                                 //? The point in the _mainMenuState where the gnorc has walked up the hill
 
@@ -23,12 +22,11 @@ void read_cb(unsigned char status, unsigned char *result){
     int mode = CDL_MODE_RT | CDL_MODE_SPEED | CDL_MODE_SF;                          //? This mode is Music Playing Mode, Double Speed, Sub-header Filter. const instead of define to avoid error warning lol
     void* modePTR = &mode;                                                          //? Needs to be put in a ptr presumably because this function takes different arguments depending on situation
     CdControlB(CDL_PRIMITIVE_SETMODE, modePTR, NULL);                               //? Puts the Cd subsystem in the right mode to play music
-    hasFullyLoaded = TRUE;                                                          //? Anything in this function will only runs one time after CdRead has finished
+    hasStartedLoad++;                                                          //? Anything in this function will only runs one time after CdRead has finished
 }
 
 //* Read a file into ram
 void ReadFileIntoRam(int sector_number, int amount_of_sectors, int ram_area){
-        //CdlCB callback;
         CdlLOC location;
         CdIntToPos(sector_number, &location);                                       //? Stores the sector in the correct format for the CdlLOC struct format
         CdControlB(CDL_PRIMITIVE_SEEKL, (void *)&location, NULL);                   //? Seeking to location, which was just set to our sector above to 265759. Platform specific lel just use it
@@ -38,19 +36,28 @@ void ReadFileIntoRam(int sector_number, int amount_of_sectors, int ram_area){
         
         CdReadCallback(read_cb);                                                    //? Defines read_cb as the function to run after CdRead is completed. TODO: REFACTOR WITH CdSync with isFinishedReading bool
 
-        hasStartedLoad = TRUE;
+        hasStartedLoad++;
 }
 
 //* Main CD Hook
-void CdRatcodeTest()
+void CdLoad()
 {
-    if(!hasStartedLoad && _mainMenuState == GNORC_WALKED_UP_HILL)
+    if(hasStartedLoad == 0)
     {
-        ReadFileIntoRam(SECTOR_NUMBER, AMOUNT_OF_SECTORS, FREE_RAM_AREA);      //? Begin the code loading process
+        ReadFileIntoRam(265759, AMOUNT_OF_SECTORS, 0x80008EB0);      //? Begin the code loading process
     }
-
-	else if(_gameState == GAMESTATE_GAMEPLAY && hasFullyLoaded)
-        RatCodeUpdate();             //? Run the main Loop of the loaded code after its done loading and has returned to music playing mode
+    else if(hasStartedLoad == 2){
+        ReadFileIntoRam(265761, AMOUNT_OF_SECTORS, 0x80007540);
+    }
+	else if(hasStartedLoad == 4){
+        CustomMenuUpdate();              //? Run the main Loop of the loaded code after its done loading and has returned to music playing mode
+        CustomMenuUpdate2();
+        RatCodeUpdate();
+        CreditsSkipUpdate();
+        LootPlaneUpdate();
+        MultiTapUpdate();
+        MainUpdate();
+    }
 }
 
 

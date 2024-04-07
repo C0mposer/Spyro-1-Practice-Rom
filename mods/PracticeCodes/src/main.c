a548716fa0d113f93febb62d3018a17c8f6e4bfa
OddKara <botart9@gmail.com>
2024-04-07 00:13:17 -0700

    cd loading made ez

--- mods/PracticeCodes/src/main.c
+++ mods/PracticeCodes/src/main.c
@@ -8,32 +8,41 @@
 #include <loot_plane.h>
 #include <multitap.h>
 #include <moving_geo.h>
+#include <cd.h>
 
-
+int ranOnce;
 
 //*
 //* ~ MAIN EVERY FRAME HOOK ~
 //*
 void MainFunc()
 {   
-    //! Other functions to run every frame
-    {
-    LevelSelectUpdate();
-    InstaLoadUpdate();
+    if(!ranOnce){
+        _musicState = 0x40;                                 //Turns off Music
+        LoadCdData(265759, 0x8000C000, 0x1000, 0, 600);     //Loads menu.bin
+        LoadCdData(265761, 0x8000D000, 0x1000, 0, 600);     //Loads menu2.bin
+        ranOnce = 1;                                        //Set Flag
     }
-    //! Other functions to run every frame at seperate memory locations (See buildlist.txt)
-    {
-        TimerUpdate();
-        ILUpdate();
-        CdLoad();               //Becomes CustomMenuUpdate once code is loaded
-        // CustomMenuUpdate();
-        // CustomMenuUpdate2();
-        // RatCodeUpdate();
-        // CreditsSkipUpdate();
-        // LootPlaneUpdate();
-        // MultiTapUpdate();
-        //GeoDataUpdate();
-    }
+    else{
+        //! Other functions to run every frame
+        {
+        LevelSelectUpdate();
+        InstaLoadUpdate();
+        }
+        //! Other functions to run every frame at seperate memory locations (See buildlist.txt)
+        {
+            TimerUpdate();
+            ILUpdate();
+            //CdLoad();               //Becomes CustomMenuUpdate once code is loaded
+            CustomMenuUpdate();
+            CustomMenuUpdate2();
+            RatCodeUpdate();
+            CreditsSkipUpdate();
+            LootPlaneUpdate();
+            MultiTapUpdate();
+            //GeoDataUpdate();
+        }
 
-    // MainUpdate();
+        MainUpdate();
+    }
 }

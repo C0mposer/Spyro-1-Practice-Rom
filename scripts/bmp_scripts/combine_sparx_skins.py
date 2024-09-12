2cb9fa8203eb516b7a2d2af5127af37c498d2618
C0mposer <78276528+C0mposer@users.noreply.github.com>
2024-09-11 23:36:20 -0700

    Update

--- scripts/bmp_scripts/combine_sparx_skins.py
+++ scripts/bmp_scripts/combine_sparx_skins.py
@@ -3,7 +3,7 @@ import os
 # Combines all converted sparx bmps to 1 combined sparx.bin file
 def CombineSparxSkins():
     directory = "bmps/"
-    file_list = ["og_sparx.bin", "red_sparx.bin", "darkpurple_sparx.bin", "orange_sparx.bin", "darkblue_sparx.bin", "pink_sparx.bin", "turquoise_sparx.bin", "silver_sparx.bin", "black_sparx.bin"]  # Add all your files here
+    file_list = ["og_sparx.bin", "red_sparx.bin", "darkpurple_sparx.bin", "orange_sparx.bin", "darkblue_sparx.bin", "pink_sparx.bin", "turquoise_sparx.bin", "silver_sparx.bin", "black_sparx.bin", "custom_sparx.bin"]  # Add all your files here
     output_file = "final_output/sparx.bin"
 
     # Define constants

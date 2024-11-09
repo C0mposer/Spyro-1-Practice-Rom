2ee92c5bcb65ec73e1c435625e1f41e96a694946
C0mposer <78276528+C0mposer@users.noreply.github.com>
2024-11-08 19:23:24 -0800

    Update

--- mods/PracticeCodes/src/custom_menu_2.c
+++ mods/PracticeCodes/src/custom_menu_2.c
@@ -856,7 +856,7 @@ void CustomMenuUpdate2()
         }
     }
 
-    if (((timer_menu.timer_display_mode == TIMER_ALWAYS || il_menu.il_timer_display_mode == IL_TIMER_ALWAYS || timerState == TIMER_DISPLAYING || menu_state == MENU_DISPLAYING || ShouldDisplayLandingTime()) && _gameState == GAMESTATE_GAMEPLAY) || (il_timer_state == IL_DISPLAYING && _gameState == GAMESTATE_LOADING) || ((il_menu.display_on_dragon == TRUE || misc_menu.show_dragon_touch == TRUE) && _gameState == GAMESTATE_DRAGON_STATE))
+    if (((timer_menu.timer_display_mode == TIMER_ALWAYS || il_menu.il_timer_display_mode == IL_TIMER_ALWAYS || timerState == TIMER_DISPLAYING || menu_state == MENU_DISPLAYING || ShouldDisplayLandingTime()) && _gameState == GAMESTATE_GAMEPLAY) || (il_timer_state == IL_DISPLAYING) || ((il_menu.display_on_dragon == TRUE || misc_menu.show_dragon_touch == TRUE) && _gameState == GAMESTATE_DRAGON_STATE))
     {
         // printf("RENDERING\n");
         RenderShadedMobyQueue();
